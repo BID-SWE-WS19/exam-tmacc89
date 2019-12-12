16e59ed3cd9dc0fa7a065bd47c2b3c566d4cf466
tm <tm@untis.at>
2019-12-12 17:18:01 +0100

    final

--- Java/src/main/java/BID/SWE/Exercise5Impl.java
+++ Java/src/main/java/BID/SWE/Exercise5Impl.java
@@ -1,10 +1,31 @@
 package BID.SWE;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.LinkedList;
+
 public class Exercise5Impl implements Exercise5
 {
 
     @Override
     public Object Method1(String s, int i) {
-        return null;
+
+        try {
+            Socket socket = new Socket(s, i);
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                LinkedList<String>.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- Java/src/main/java/BID/SWE/Exercise6Impl.java
+++ Java/src/main/java/BID/SWE/Exercise6Impl.java
@@ -1,8 +1,8 @@
 package BID.SWE;
 
+
 public class Exercise6Impl implements Exercise6
 {
-
     @Override
     public Object Method1() {
         return null;
